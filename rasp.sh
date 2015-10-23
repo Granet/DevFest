a6020e5a706ece4006f0de55b1e8b58f8404c173
Granet <granet@inventati.org>
2015-10-23 16:27:24 +0200

    Update rasp.sh

--- rasp.sh
+++ rasp.sh
@@ -3,39 +3,39 @@
 
 #Prima parte
 
-echo 2 > /sys/class/gpio/export
-echo 3 > /sys/class/gpio/export
 echo 4 > /sys/class/gpio/export
+echo 17 > /sys/class/gpio/export
+echo 27 > /sys/class/gpio/export
+echo 22 > /sys/class/gpio/export
 echo 5 > /sys/class/gpio/export
 echo 6 > /sys/class/gpio/export
-echo 7 > /sys/class/gpio/export
-echo 8 > /sys/class/gpio/export
-echo 9 > /sys/class/gpio/export
-echo 10 > /sys/class/gpio/export
-echo 11 > /sys/class/gpio/export
-echo 12 > /sys/class/gpio/export
 echo 13 > /sys/class/gpio/export
-echo 14 > /sys/class/gpio/export
-echo 15 > /sys/class/gpio/export
+echo 19 > /sys/class/gpio/export
+echo 26 > /sys/class/gpio/export
+echo 18 > /sys/class/gpio/export
+echo 23 > /sys/class/gpio/export
+echo 24 > /sys/class/gpio/export
+echo 25 > /sys/class/gpio/export
+echo 12 > /sys/class/gpio/export
 echo 16 > /sys/class/gpio/export
 
 
 #Out/In
 
-echo out > /sys/class/gpio/gpio2/direction
-echo out > /sys/class/gpio/gpio3/direction
 echo out > /sys/class/gpio/gpio4/direction
+echo out > /sys/class/gpio/gpio17/direction
+echo out > /sys/class/gpio/gpio27/direction
+echo out > /sys/class/gpio/gpio22/direction
 echo out > /sys/class/gpio/gpio5/direction
 echo out > /sys/class/gpio/gpio6/direction
-echo out > /sys/class/gpio/gpio7/direction
-echo out > /sys/class/gpio/gpio8/direction
-echo out > /sys/class/gpio/gpio9/direction
-echo out > /sys/class/gpio/gpio10/direction
-echo out > /sys/class/gpio/gpio11/direction
-echo out > /sys/class/gpio/gpio12/direction
 echo out > /sys/class/gpio/gpio13/direction
-echo out > /sys/class/gpio/gpio14/direction
-echo out > /sys/class/gpio/gpio15/direction
+echo out > /sys/class/gpio/gpio19/direction
+echo out > /sys/class/gpio/gpio26/direction
+echo out > /sys/class/gpio/gpio18/direction
+echo out > /sys/class/gpio/gpio23/direction
+echo out > /sys/class/gpio/gpio24/direction
+echo out > /sys/class/gpio/gpio25/direction
+echo out > /sys/class/gpio/gpio12/direction
 echo out > /sys/class/gpio/gpio16/direction
 
 
@@ -53,104 +53,102 @@ for i in "$@"
 	do
 		case $i in
 		    --salotto)
-		    	echo 1 > /sys/class/gpio/gpio2/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio2/value
-		    	exit
-		    ;;
-
-		    --cucina)
-		    	echo 1 > /sys/class/gpio/gpio3/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio3/value
-		    	exit
-		    ;;
-
-		    --bagno)
 		    	echo 1 > /sys/class/gpio/gpio4/value
 		    	sleep 0.5
 		    	echo 0 > /sys/class/gpio/gpio4/value
 		    	exit
 		    ;;
+
+		    --cucina)
+		    	echo 1 > /sys/class/gpio/gpio17/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio17/value
+		    	exit
+		    ;;
+
+		    --bagno)
+		    	echo 1 > /sys/class/gpio/gpio27/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio27/value
+		    	exit
+		    ;;
 		    
 		    --matrimoniale)
+		    	echo 1 > /sys/class/gpio/gpio22/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio22/value
+		    	exit
+		    ;;
+		    
+		    --letto)
 		    	echo 1 > /sys/class/gpio/gpio5/value
 		    	sleep 0.5
 		    	echo 0 > /sys/class/gpio/gpio5/value
 		    	exit
 		    ;;
 		    
-		    --letto)
+		    --tv)
 		    	echo 1 > /sys/class/gpio/gpio6/value
 		    	sleep 0.5
 		    	echo 0 > /sys/class/gpio/gpio6/value
 		    	exit
 		    ;;
 		    
-		    --tv)
-		    	echo 1 > /sys/class/gpio/gpio7/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio7/value
-		    	exit
-		    ;;
-		    
 		    --tutteluci)
-		    	echo 1 > /sys/class/gpio/gpio8/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio8/value
-		    	exit
-		    ;;
-		    
-		    --luceesterne)
-		    	echo 1 > /sys/class/gpio/gpio9/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio9/value
-		    	exit
-		    ;;
-		    
-		    --tapparellaon1)
-		    	echo 1 > /sys/class/gpio/gpio10/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio10/value
-		    	exit
-		    ;;
-		    
-		    --tapparellaoff1)
-		    	echo 1 > /sys/class/gpio/gpio11/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio11/value
-		    	exit
-		    ;;
-		    
-		    --tapparellaon2)
-		    	echo 1 > /sys/class/gpio/gpio12/value
-		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio12/value
-		    	exit
-		    ;;
-		    
-		    --tapparellaoff2)
 		    	echo 1 > /sys/class/gpio/gpio13/value
 		    	sleep 0.5
 		    	echo 0 > /sys/class/gpio/gpio13/value
 		    	exit
 		    ;;
 		    
-		    --tapparellegeneraleon)
-		    	echo 1 > /sys/class/gpio/gpio14/value
+		    --luceesterne)
+		    	echo 1 > /sys/class/gpio/gpio19/value
 		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio14/value
+		    	echo 0 > /sys/class/gpio/gpio19/value
+		    	exit
+		    ;;
+		    
+		    --tapparellaon1)
+		    	echo 1 > /sys/class/gpio/gpio26/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio26/value
+		    	exit
+		    ;;
+		    
+		    --tapparellaoff1)
+		    	echo 1 > /sys/class/gpio/gpio18/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio18/value
+		    	exit
+		    ;;
+		    
+		    --tapparellaon2)
+		    	echo 1 > /sys/class/gpio/gpio23/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio23/value
+		    	exit
+		    ;;
+		    
+		    --tapparellaoff2)
+		    	echo 1 > /sys/class/gpio/gpio24/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio24/value
+		    	exit
+		    ;;
+		    
+		    --tapparellegeneraleon)
+		    	echo 1 > /sys/class/gpio/gpio25/value
+		    	sleep 0.5
+		    	echo 0 > /sys/class/gpio/gpio25/value
 		    	exit
 		    ;;
 
 		    --tapparellegeneraleoff)
-		    	echo 1 > /sys/class/gpio/gpio15/value
+		    	echo 1 > /sys/class/gpio/gpio12/value
 		    	sleep 0.5
-		    	echo 0 > /sys/class/gpio/gpio15/value
+		    	echo 0 > /sys/class/gpio/gpio12/value
 		    	exit
 		    ;;
-		    
-		    
 		    
 		    --allarme)
 		    	echo 1 > /sys/class/gpio/gpio16/value
